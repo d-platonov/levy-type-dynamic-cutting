13bbc1a3be77589905de62b3668ae6d9abd2935f
Denis Platonov <dplatonov.ua@gmail.com>
2024-12-07 17:11:30 +0200

    Remove additional initializing of rng and config

--- levy_simulator.py
+++ levy_simulator.py
@@ -10,15 +10,13 @@ warnings.filterwarnings("ignore")
 class LevySimulator:
     def __init__(self, process: LevyTypeProcess):
         self.process = process
-        self.config = process.config
-        self.rng = process.rng
 
     def prepare_timeline(self) -> tuple[np.ndarray, dict[np.ndarray, np.ndarray]]:
         """Prepare time points and jumps for simulation."""
-        times = np.linspace(0, self.config.total_time, self.config.num_steps)
+        times = np.linspace(0, self.process.config.total_time, self.process.config.num_steps)
         jumps_dict = {}
         for sign in (1, -1):
-            jump_times = self.process.generate_jump_times(self.config.total_time)
+            jump_times = self.process.generate_jump_times(self.process.config.total_time)
             jumps_dict.update({t: self.process.sample_large_jump(t, sign) for t in jump_times})
             times = np.append(times, jump_times)
         return np.sort(times), jumps_dict
@@ -27,7 +25,7 @@ class LevySimulator:
         """Simulate a single path of the Levy-type process."""
         times, jumps_dict = self.prepare_timeline()
         values = np.zeros(len(times))
-        values[0] = self.config.x_0
+        values[0] = self.process.config.x_0
 
         for i in range(1, len(times)):
             dt = times[i] - times[i - 1]
@@ -40,11 +38,11 @@ class LevySimulator:
             diffusion = (
                 self.process.diffusion_coefficient(t_prev, x_prev)
                 * np.sqrt(dt)
-                * self.rng.standard_normal()
+                * self.process.rng.standard_normal()
             )
 
             small_jump_var = self.process.compute_small_jump_variance(x_prev, t_prev, t_curr)
-            small_jump_increment = (np.sqrt(small_jump_var) * self.rng.standard_normal())
+            small_jump_increment = (np.sqrt(small_jump_var) * self.process.rng.standard_normal())
 
             if t_curr in jumps_dict:
                 large_jump_increment = self.process.jump_coefficient(t_prev, x_prev, jumps_dict.get(t_curr))
